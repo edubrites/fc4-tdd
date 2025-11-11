2f59d3f3170692e804126bc6fa824dab930d9668
edubrites <egbrites@gmail.com>
2025-11-11 00:33:15 -0300

    add testes E2E de Criação de Usuário

--- src/application/services/user_service.ts
+++ src/application/services/user_service.ts
@@ -1,9 +1,24 @@
-import { User } from "../../domain/entities/user";
-import { UserRepository } from "../../domain/repositories/user_repository";
+import {User} from "../../domain/entities/user";
+import {UserRepository} from "../../domain/repositories/user_repository";
+import {CreateUserDTO} from "../dtos/create_user_dto";
 
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
-  async findUserById(id: string): Promise<User | null> {
-    return this.userRepository.findById(id);
-  }
+    constructor(
+        private readonly userRepository: UserRepository
+    ) {}
+
+    async findUserById(id: string): Promise<User | null> {
+        return this.userRepository.findById(id);
+    }
+
+    async createUser(dto: CreateUserDTO): Promise<User> {
+
+        const user = new User(
+            dto.id,
+            dto.name
+        );
+
+        await this.userRepository.save(user);
+        return user;
+    }
 }
